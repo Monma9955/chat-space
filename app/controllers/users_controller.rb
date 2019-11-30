e45ea1e71acb87205875a23edcb58c513c4e25b0
Monma_Takeshi <55307855+Monma9955@users.noreply.github.com>
2019-11-30 16:26:15 +0900

    Update users_controller.rb
    updateアクションの追記をしました。
    if文でcurrent_user.update(user_params)がTrueの場合はルートパスにリダイレクト、Falseの場合はeditアクションのビューを再描画するようにしました。
    
    user_paramsメソッドの値はプライベートメソッド配下に記述しました。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,16 @@ class MessagesController < ApplicationController
   end
 
   def update
+    if current_user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit
     end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).parmit(:name, :email)
   end
 end
