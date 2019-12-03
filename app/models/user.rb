c0d7767bf7219e02e25ee99cda8bf2995de79050
Monma_Takeshi <55307855+Monma9955@users.noreply.github.com>
2019-12-04 00:21:00 +0900

    Modify user.rb
    user.rbのアソシエーションでグループ⇔ユーザーの表記が誤っていたため、修正しました。

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  has_many :groups_users
-  has_many :groups, through: :groups_users
+  has_many :group_users
+  has_many :groups, through: :group_users
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
