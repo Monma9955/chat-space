a230ad0e2e6adccefc491bb76f5f66d14ec4226d
Monma_Takeshi <55307855+Monma9955@users.noreply.github.com>
2019-12-06 22:47:30 +0900

    Remove edit from groups_controller
    グループ編集機能作成。
    グループコントローラーのeditアクション内の記述はbefore_actionのset_groupだけで事足りるため、editアクションの記載をコントローラーから削除しました。

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -12,13 +12,10 @@ class GroupsController < ApplicationController
   def create
     @group = Group.new(group_params)
     if @group.save
-      redirect_to root_path, notice: 'グループを作成しました'
+      redirect_to root_path, notice: "グループを作成しました"
     else
       render :new
     end
-  end
-
-  def edit
   end
 
   def update
