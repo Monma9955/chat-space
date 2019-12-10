8c71142b33972c33f24d2ccfeb5e440ab277d7eb
Monma_Takeshi <jemini0527@gmail.com>
2019-12-10 23:31:41 +0900

    Update messages_controller.rb
    メッセージ投稿機能の作成。
    
    private以下にビューから送られてくる情報を受け取るmessage_paramsと現在表示されているビューのグループ情報を格納するための@groupを定義したset_groupメソッドを作成。
    
    set_groupはそれぞれのアクションで使用できるようbefore_actionに設定しました。

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,17 @@
 class MessagesController < ApplicationController
+  before_action :set_group
+
   def index
   end
 
   def create
+  private
+
+  def message_params
+    params.require(:message).permit(:message, :image).merge(user_id: current_user.id)
+  end
+
+  def set_group
+    @group = Group.find(params[:group_id])
   end
 end
