b073741d3937b9ba8390972161bc6c5ef44fd09a
Monma_Takeshi <jemini0527@gmail.com>
2019-12-23 17:50:07 +0900

    Add message.js
    フォームのメッセージをJSON形式で送信することに成功した場合、以下のイベントが実行されるようにしました。
    
    ・送信した内容が今までのHTML構成に則ってチャットの一番下に表示される
    ・チャット画面の一番下にスクロールされる
    ・フォームのテキストと画像がリセットされる
    ・連続でフォームのボタンが押せるようになる

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,44 @@
 $(function(){
+  function buildHTML(message) {
+    if( message.image ) {
+      var html = 
+      `<div class='message'>
+        <div class='message__upper-info'>
+          <div class='message__upper-info__talker'>
+            ${message.user_name}
+          </div>
+          <div class='message__upper-info__date'>
+            ${message.created_at}
+          </div>
+        </div>
+        <div class='message__lower'>
+          <p class='message__lower__text'>
+            ${message.message}
+          </p>
+          <img class="message__lower__image" src=${message.image}>
+        </div>
+      </div>`
+      return html;
+    } else {
+      var html =
+       `<div class='message'>
+          <div class='message__upper-info'>
+            <div class='message__upper-info__talker'>
+              ${message.user_name}
+            </div>
+            <div class='message__upper-info__date'>
+              ${message.created_at}
+            </div>
+          </div>
+          <div class='message__lower'>
+            <p class='message__lower__text'>
+              ${message.message}
+            </p>
+          </div>
+        </div>`
+      return html;
+    };
+  }
   $('#new_message').on('submit', function(e) {
     e.preventDefault();
     var formData = new FormData(this);
@@ -11,6 +51,13 @@ $(function(){
       dataType: 'json',
       processData: false,
       contentType: false
+    })
+    .done(function(data) {
+      var html = buildHTML(data);
+      $('.messages').append(html);
+      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+      $('#new_message')[0].reset();
+      $('.form__NewMessage__submit-btn').prop('disabled', false);
     })
   })
 })
