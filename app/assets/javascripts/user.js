17c3367eb1425c81efd303f4f351dc26cccfe492
Monma_Takeshi <jemini0527@gmail.com>
2019-12-25 00:10:51 +0900

    Add user.js pattern done and fail
    フォームでのユーザー検索が成功した場合と失敗した場合の処理をjsファイルに記述。
    
    成功した場合は一旦検索結果のフォームを空にしてからif分岐を実行。
    
    if分岐により文字数が0でなければ一致する検索結果を一覧で表示、0なら返り値はfalse、それ以外はユーザーが見つからなかった旨のHTML情報を表示させるようにしました。
    
    失敗した場合はユーザー検索に失敗した旨のアラートを表示させました。

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,4 +1,27 @@
 $(function() {
+
+  var search_result = $('#user-search-result')
+
+  function appendUser(user) {
+    var html =`
+              <div class='chat-group-user clearfix'>
+                <p class='chat-group-user__name'>${user.name}</p>
+                <div class='user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
+              </div>
+              `
+    search_result.append(html);
+    console.log(html);
+  }
+
+  function FormNoUser() {
+    var html =`
+              <div class="chat-group-user clearfix">
+              <p class="chat-group-user__name">ユーザーが見つかりません</p>
+              </div>`
+    search_result.append(html);
+    console.log(html);
+  }
+
   $('#user-search-field').on('keyup', function(){
     var input = $('#user-search-field').val();
     $.ajax({
@@ -7,5 +30,20 @@ $(function() {
       data: { keyword: input },
       dataType: 'json'
     })
+    .done(function(users) {
+      search_result.empty();
+      if (users.length !== 0) {
+        users.forEach(function(user) {
+          appendUser(user);
+        });
+      } else if (input.length == 0) {
+        return false;
+      } else {
+        FormNoUser();
+      }
+    })
+    .fail(function() {
+      alert("ユーザー検索に失敗しました");
+    });
   });
 });
