1bece40499ab0d58b27f73541a80064a556f4158
Monma_Takeshi <jemini0527@gmail.com>
2019-12-25 14:57:26 +0900

    Modify user.js
    jsファイルの記述を細かく修正。
    
    ・「"」で記載していた部分を一部「'」に変更
    ・メソッド名を中の処理がわかりやすくなるよう変更
    ・divタグで記述していた部分をaタグに変更

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -2,21 +2,21 @@ $(function() {
 
   var search_result = $('#user-search-result')
 
-  function appendUser(user) {
+  function hitUser(user) {
     var html =`
               <div class='chat-group-user clearfix'>
                 <p class='chat-group-user__name'>${user.name}</p>
-                <div class='user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
+                <a class='user-search-add chat-group-user__btn chat-group-user__btn--add' data-user-id='${user.id}' data-user-name='${user.name}'>追加</a>
               </div>
-              `
+              `;
     search_result.append(html);
   }
 
-  function FormNoUser() {
+  function hitNoUser() {
     var html =`
-              <div class="chat-group-user clearfix">
-              <p class="chat-group-user__name">ユーザーが見つかりません</p>
-              </div>`
+              <div class='chat-group-user clearfix'>
+              <p class='chat-group-user__name'>ユーザーが見つかりません</p>
+              </div>`;
     search_result.append(html);
   }
 
@@ -32,12 +32,12 @@ $(function() {
       search_result.empty();
       if (users.length !== 0) {
         users.forEach(function(user) {
-          appendUser(user);
+          hitUser(user);
         });
       } else if (input.length == 0) {
         return false;
       } else {
-        FormNoUser();
+        hitNoUser();
       }
     })
     .fail(function() {
