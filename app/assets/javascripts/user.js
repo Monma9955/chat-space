55800b781d19a27adbabb5d94663cdec9f49228e
Monma_Takeshi <jemini0527@gmail.com>
2019-12-25 15:05:02 +0900

    Add user.js add member function
    インクリメンタルサーチで表示されたメンバーをチャットグループのメンバーに追加できるようにしました。
    
    以下の挙動をjsファイルに記述しました。
    ・検索結果の「追加」ボタンをクリックしたら発火
    ・クリックされたユーザーのHTML要素からユーザーの名前とIDを取得(変数に格納)
    ・グループのメンバー一覧にクリックされたユーザーをHTML要素で追加
    ・メンバー一覧に追加されたメンバーはインクリメンタルサーチの検索結果からは削除する

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -20,6 +20,17 @@ $(function() {
     search_result.append(html);
   }
 
+  function  addUser(name, id){
+    var html = `
+            <div class='chat-group-user'>
+              <input name='group[user_ids][]' type='hidden' value='${id}'>
+              <p class='chat-group-user__name'>${name}</p></p>
+              <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+            </div>
+            `
+    $('#chat-group-users').append(html)
+  };
+
   $('#user-search-field').on('keyup', function(){
     var input = $('#user-search-field').val();
     $.ajax({
@@ -43,5 +54,12 @@ $(function() {
     .fail(function() {
       alert("ユーザー検索に失敗しました");
     });
+  });
+
+  $(document).on('click', '.chat-group-user__btn--add', function(){
+    var userName = $(this).attr('data-user-name');
+    var userId = $(this).attr('data-user-id');
+    addUser(userName, userId);
+    $(this).parent().remove();
   });
 });
