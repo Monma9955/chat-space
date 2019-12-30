c58028c0ce8dd28b4d29ee7fa8a53f455103c6ae
Monma_Takeshi <jemini0527@gmail.com>
2019-12-30 16:23:45 +0900

    Update application.rb
    投稿時間を日本時間にするため、application.rbにタイムゾーンの設定をしました。

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,7 @@ module ChatSpace
     end
     config.action_view.field_error_proc = Proc.new { |html_tag, instance| html_tag }
     config.i18n.default_locale = :ja
+    config.time_zone = 'Tokyo'
   end
 end
 
